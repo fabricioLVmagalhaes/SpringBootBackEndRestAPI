669da576d90bb0382ec211eb674e7293e7fbd393
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2018-08-06 11:20:55 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/fabriciomagalhaes/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.fabriciomagalhaes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabriciomagalhaes.cursomc.domain.Categoria;
+import com.fabriciomagalhaes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id){
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+		
+	}
+
+}
