163b19ef64e70c0997224de3a1c1fc9480ada336
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2018-08-10 10:10:04 -0300

    Paginacao com parametos adicionais na requisicao

--- src/main/java/com/fabriciomagalhaes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fabriciomagalhaes/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -69,5 +71,19 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	//endpoint para busca de lista de categorias || All categories' endpoint
+		@GetMapping(value="/page")
+		public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value="page", defaultValue="0") Integer page, 
+				                                           @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+				                                           @RequestParam(value="borderBy", defaultValue="nome") String orderBy, 
+				                                           @RequestParam(value="direction", defaultValue="ASC") String direction) {
+			
+			Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+			//metodo para converção da Page<Categoria> para List<CategoriaDTO> ||Page<Categoria> to List<CategoiraDTO> conversion method
+			Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
+			
+			return ResponseEntity.ok().body(listDTO);
+		}
 
 }
