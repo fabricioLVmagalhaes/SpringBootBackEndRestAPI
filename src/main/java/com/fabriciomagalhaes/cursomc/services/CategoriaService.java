2bcc00361591f2e01093cdc63d2fc08fa9e2d995
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2018-08-10 13:17:02 -0300

    Correcoes: update em categorias e construtor de pagamento

--- src/main/java/com/fabriciomagalhaes/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabriciomagalhaes/cursomc/services/CategoriaService.java
@@ -37,16 +37,17 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		} 
-		
+		}
+
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 
@@ -55,19 +56,24 @@ public class CategoriaService {
 	}
 
 	public List<Categoria> findAll() {
-		
+
 		return repo.findAll();
 	}
-	
-	//medodo para retornar categorias com paginação
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		
+
+	// medodo para retornar categorias com paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+
 	}
 
 }
